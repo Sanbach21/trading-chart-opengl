a8050aaadc044eb242d5ac8a23e2857ea98c7848
Sanbach21 <sanbach21@aol.com>
2026-05-17 01:20:39 -0700

    actualizacion del Grid

--- charts/overlays/grid.py
+++ charts/overlays/grid.py
@@ -102,38 +102,32 @@ class GridOverlay:
                     width=self.style.major_width,
                 )
 
-        # ======================================================
+               # ======================================================
         # GRID VERTICAL
         # ======================================================
 
         if self.style.show_vertical:
 
             tick_indices = self.time_scale.get_tick_indices(
-                        min_spacing_px=self.style.vertical_min_spacing_px,
-                        extend_by_one=True,
-                    )
-            right_limit = plot_x + plot_w 
+                min_spacing_px=self.style.vertical_min_spacing_px,
+                extend_by_one=True,
+            )
+
+            plot_right = plot_x + plot_w
 
             for i in tick_indices:
-
                 x = self.time_scale.get_aligned_x(
-                    i,
-                    crisp=self.style.crisp_vertical_lines,
+                    i, 
+                    crisp=self.style.crisp_vertical_lines
                 )
 
-                # fuera del área visible izquierda
-                if x < plot_x - 20:
-                    continue
-
-                # fuera del chart derecho real
-                if x > right_limit:
-                    continue
-
-                renderer.draw_line_px(
-                    x,
-                    plot_y,
-                    x,
-                    plot_y + plot_h,
-                    color=self.style.vertical_major_color,
-                    width=self.style.vertical_major_width,
-                )
+                # Solo dibujar dentro del área del gráfico
+                if plot_x - 5 <= x <= plot_right + 5:
+                    renderer.draw_line_px(
+                        x,
+                        plot_y,
+                        x,
+                        plot_y + plot_h,
+                        color=self.style.vertical_major_color,
+                        width=self.style.vertical_major_width,
+                    )
